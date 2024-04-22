edf89462ba5ba9fb149d3df2bfb0698f57c74bb1
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-04-22 23:39:47 +0900

    chore: --lint=fixで自動修正するように変更

--- jest-runner-eslint.config.js
+++ jest-runner-eslint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     cliOptions: {
-        // npm test --eslint-fixで実行すると修正可能な場所は自動的に修正される
-        fix: process.env.npm_config_eslint_fix !== undefined,
+        // npm test --lint=fixで実行すると修正可能な場所は自動的に修正される
+        fix: true, //process.env.npm_config_lint === 'fix',
     },
 };
