87248f4264d87ce3e02d7a5f8825b24e4e62a63b
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-04-22 23:41:34 +0900

    fix: indentedで先頭末尾の改行除去はテンプレートのものだけになるように修正

--- tests/testing-library/indented.ts
+++ tests/testing-library/indented.ts
@@ -6,19 +6,41 @@ export function indented(
     const [{ raw: template }] = args;
     // 最後の改行から終端までが空白とタブだけであればその空白とタブの連続をインデントと見なす
     const indent = /\n[ \t]+$/.exec(template[template.length - 1])?.[0];
-    const unindent: (t: string) => string =
+    const modifier =
         indent && template[0].startsWith(indent)
             ? // 先頭が終端と同じ改行とインデントで始まっていればインデント除去とエスケープ解除
-              (t) => unescape(t.replaceAll(indent, '\n'))
+              unindentAndUnescape(template.length, indent)
             : // でなければエスケープ解除のみ
-              (t) => unescape(t);
+              unescape;
     return (
         template
             // インデント除去
-            .map(unindent)
+            .map(modifier)
             // テンプレートリテラルを連結
             .reduce((r, e, i) => r.concat(String(args[i]), e))
             // 先頭と終端の改行は削除
             .replace(/^\n|\n$/g, '')
+    );
+}
+
+function unindentAndUnescape(length: number, indent: string) {
+    return (t: string, i: number) =>
+        unescape(trimNewLine(t.replaceAll(indent, '\n'), i, length));
+}
+
+function trimNewLine(t: string, i: number, length: number): string {
+    return t.replace(
+        length === 1
+            ? // templateが一つしかない場合、先頭と終端両方を除去する
+              /^\n|\n$/g
+            : i === 0
+              ? // 先頭の改行を除去
+                /^\n/
+              : i === length - 1
+                ? // 終端の改行を除去
+                  /\n$/
+                : // 改行を消す必要がない場合は置換が発生しないパターンにする
+                  /^$/,
+        '',
     );
 }
