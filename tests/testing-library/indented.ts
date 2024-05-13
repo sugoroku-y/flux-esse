e760132b779b20e65cde63a740258f55630c8c0b
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-05-14 00:05:23 +0900

    refactor: 先頭、末尾の改行を除去する処理をシンプルなものに変更

--- tests/testing-library/indented.ts
+++ tests/testing-library/indented.ts
@@ -32,18 +32,14 @@ function unescapeOnly(template: readonly string[]) {
 }
 
 function trimNewLine(t: string, i: number, length: number): string {
-    return t.replace(
-        length === 1
-            ? // templateが一つしかない場合、先頭と終端両方を除去する
-              /^\n|\n$/g
-            : i === 0
-              ? // 先頭の改行を除去
-                /^\n/
-              : i === length - 1
-                ? // 終端の改行を除去
-                  /\n$/
-                : // 改行を消す必要がない場合は絶対にマッチしないパターンにする
-                  /^(?=.)$/,
-        '',
-    );
+    let s = t;
+    if (i === 0) {
+        // 先頭の改行を除去
+        s = s.replace(/^(?:\r\n|[\r\n\u2028\u2029])/, '');
+    }
+    if (i === length - 1) {
+        // 終端の改行を除去
+        s = s.replace(/(?:\r\n|[\r\n\u2028\u2029])$/, '');
+    }
+    return s;
 }
