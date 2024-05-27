4fd72edda23d78ceb8b3d6adcba99924de3d92be
YEBISUYA Sugoroku <yebisuya@gmail.com>
2024-05-27 23:27:23 +0900

    chore: jest実行時にprettierでの確認も行うように

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,12 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
     projects: [
         '<rootDir>/jest-test.config.ts',
-        ...(process.env.npm_config_lint ? ['<rootDir>/jest-eslint.config.ts'] : []),
+        ...(process.env.npm_config_lint
+            ? [
+                  '<rootDir>/jest-eslint.config.ts',
+                  { preset: '@sugoroku-y/jest-runner-prettier' },
+              ]
+            : []),
     ],
     collectCoverage: !!process.env.npm_config_coverage,
 };
